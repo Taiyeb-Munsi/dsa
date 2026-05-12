91362ce13abdd422b4324726ec743f3afb9da6d5
Taiyeb Munsi <taiyebmunsi@gmail.com>
2026-05-12 17:32:11 +0530

    Linear Search

---
+++ src/searching-algorithm/LinearSearch.java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+    static int linearsearch(int[] arr, int key) {
+        for(int i=0;i<arr.length;++i) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {42, 7, 19, 88, 5, 33, 71, 12, 11, 42, 9, 24, 0, 3};
+        int target = 11;
+
+        int result = linearsearch(list, target);
+
+        if(result == -1) {
+            System.out.println("Key not found");
+        } else {
+            System.out.println("Found at index : " + result);
+        }
+    }
+}
